50413c750a76c91f47cdd821b2d176ef23dbff9a
Tony <65500763+bit-burger@users.noreply.github.com>
2024-06-14 16:45:24 +0200

    improve start of bot

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import pathlib
+from time import time
 from datetime import datetime
 import socket
 import os
@@ -9,9 +10,9 @@ import discord
 
 from config import client, token
 
-embed = discord.Embed(title="Restarted bot",
-                      description=f"Started from: `{socket.gethostname()}`\nFrom directory: `{pathlib.Path().resolve()}`",
-                      colour=0x00b0f4)
+module_str = ""
+status_str = ""
+time_to_load_str = ""
 
 for file in sorted(os.listdir(os.fsencode("features"))):
     filename = os.fsdecode(file)
@@ -24,26 +25,47 @@ for file in sorted(os.listdir(os.fsencode("features"))):
             filename = filename[0:-3]
         else:
             continue
+
+    module_str += f"`{filename}`" + "\n"
+    begin_time = time()
     try:
         importlib.import_module(f"features.{filename}")
-        embed.add_field(name=filename, value="`loaded successfully`")
+        status_str += "`loaded successfully`" + "\n"
         print(f"module '{filename}' loaded successfully")
     except BaseException as e:
-        embed.add_field(name=filename, value=f"error: `{e}`")
+        status_str += f"`loaded unsuccessful with error: {e}`" + "\n"
         print(f"module '{filename}' could not be loaded, error: {e}")
+    finally:
+        time_to_load_str += f"`{(time() - begin_time) * 1000:.3f}msec`\n"
+
+has_connected = False
 
 
 @client.event
 async def ready():
-    print(f'{client.user} has connected to Discord!')
-
+    global has_connected
     guild: discord.Guild = client.get_guild(1205582028905648209)
     channel = guild.get_channel(1247244679381254226)
+    if not has_connected:
+        print(f'{client.user} has connected to Discord!')
 
-    embed.timestamp = datetime.now()
-    embed.set_author(name=client.user.name, icon_url=client.user.display_avatar.url)
-    embed.set_footer(text=client.user.name)
-    await channel.send(embed=embed)
+        embed = discord.Embed(title="Restarted bot",
+                              description=f"**  From machine**: `{socket.gethostname()}`\n**From directory**: `{pathlib.Path().resolve()}`",
+                              colour=0x00b0f4,
+                              timestamp=datetime.now())
+        embed.add_field(name="module", value=module_str)
+        embed.add_field(name="status", value=status_str)
+        embed.add_field(name="time to load", value=time_to_load_str)
 
-
+        embed.set_author(name=client.user.name, icon_url=client.user.display_avatar.url)
+        embed.set_footer(text=client.user.name)
+        await channel.send(embed=embed)
+        has_connected = True
+    else:
+        print(f'{client.user} reconnected to Discord!')
+        embed = discord.Embed(title="Reconnected bot",
+                              description=f"**  From machine**: `{socket.gethostname()}`\n**From directory**: `{pathlib.Path().resolve()}`",
+                              colour=0x00b0f4,
+                              timestamp=datetime.now())
+        await channel.send(embed=embed)
 client.run(token)
