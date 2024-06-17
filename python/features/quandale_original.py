fdfe28a66634ddb72577de2c8d5a28c0e46fc0f0
leartpro <70581914+leartpro@users.noreply.github.com>
2024-06-17 19:25:27 +0200

    fixorino

--- python/features/quandale_original.py
+++ python/features/quandale_original.py
@@ -4,19 +4,21 @@ import json
 from config import tree
 
 with open('resources/quandale_lore.json', 'r') as f:
-    lore = json.load(f)
+    lore_data = json.load(f)
 
 with open('resources/quandale_police_report.json', 'r') as f:
-    report = json.load(f)
+    report_data = json.load(f)
 
 
 @tree.command(name="lore", description="Quandale lore", guild=discord.Object(1205582028905648209))
 async def lore(interaction: discord.Interaction):
-    for text in lore:
-        await interaction.response.send_message(text)
+    await interaction.response.send_message(lore_data[0])
+    for text in lore_data[1:]:
+        await interaction.followup.send(text)
 
 
 @tree.command(name="police_report", description="Quandale Police Report", guild=discord.Object(1205582028905648209))
 async def police_report(interaction: discord.Interaction):
-    for entry in report:
-        await interaction.response.send_message(entry)
+    await interaction.response.send_message(report_data[0])
+    for entry in report_data[1:]:
+        await interaction.followup.send(entry)
