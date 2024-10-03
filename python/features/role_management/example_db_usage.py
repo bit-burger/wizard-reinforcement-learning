747497a3af5452611ab36a357585acc64465af8d
leartpro <70581914+leartpro@users.noreply.github.com>
2024-10-03 13:14:00 +0200

    better feature organisation

---
+++ python/features/role_management/example_db_usage.py
@@ -0,0 +1,20 @@
+from features.role_management.role_database import Database
+
+db = Database()
+
+# Einen Tag mit Mitgliedern einfügen
+db.insert_tag('Tag1', '#FF5733', [708227359916163137, 7082273889161343137])
+
+# Einen Tag löschen
+db.delete_tag('Tag1')
+
+# Eine Rolle einfügen
+db.insert_role('Admin')
+
+# Den Timestamp der Rolle aktualisieren
+db.update_role_last_used('Admin')
+
+# Eine Rolle löschen
+db.delete_role('Admin')
+
+db.close()
