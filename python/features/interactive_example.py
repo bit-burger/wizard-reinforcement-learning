146cba3bb50e1af36456825de3d6662d1e57f262
Tony Borchert <65500763+bit-burger@users.noreply.github.com>
2024-10-05 02:12:36 +0200

    edit reactive

--- python/features/interactive_example.py
+++ python/features/interactive_example.py
@@ -10,14 +10,17 @@ from helpers.reactive import ReactiveApplicationView, run_application, Button
 
 class InteractiveApplication(ReactiveApplicationView):
     fib: Tuple[int, int]
+    button = True
 
     def __init__(self):
         super().__init__(ephemeral=True)
         self.fib = (0, 1)
 
-    async def fibonnaci(self, i, b):
-        self.fib = (self.fib[1], self.fib[0] + self.fib[1])
-        await self.set_state(i)
+    async def fibonnaci(self):
+        while 1:
+            self.fib = (self.fib[1], self.fib[0] + self.fib[1])
+            await self.set_state()
+            await asyncio.sleep(0.75)
 
     def render(self) -> Iterator[str | discord.Embed | ui.Item]:
         yield str(self.fib[0])  # text beispiel
@@ -28,7 +31,8 @@ class InteractiveApplication(ReactiveApplicationView):
         embed.add_field(name="assdfasdsafasdfaassdfas", value="assdfasdsafasdfaassdfasd")
         embed.add_field(name="assdfasdsafasdfaassdfas", value="assdfasdsafasdfaassdfasd")
         yield embed
-        yield Button(label="miaow", _callable=self.fibonnaci)  # button beispiel
+        if self.button:
+            yield Button(label="miaow", _callable=self.fibonnaci)  # button beispiel
 
 @tree.command(name="interactive_example", description="Startet eine Wizard Runde",
               guild=discord.Object(1205582028905648209))
--- python/helpers/reactive.py
+++ python/helpers/reactive.py
@@ -1,4 +1,5 @@
-from typing import Callable, Awaitable, Any, Iterator, Optional
+from abc import abstractclassmethod, abstractmethod
+from typing import Callable, Awaitable, Any, Iterator, Optional, final
 
 from discord import ui
 import discord
@@ -6,26 +7,31 @@ from discord._types import ClientT
 from discord.abc import MISSING
 
 
-class Select(ui.Select):
-    def __init__(self, _callable: Callable[[discord.Interaction, discord.ui.Select], Awaitable[None]],
-                 custom_id: str = MISSING,
-                 placeholder: str | None = None,
-                 min_values: int = 1,
-                 max_values: int = 1,
-                 options: list[discord.SelectOption] = MISSING,
-                 disabled: bool = False,
-                 row: int | None = None):
+class ReactiveComponent:
+    def __init__(self) -> None:
+        self.ref = None
+
+    def set_ref(self, ref) -> None:
+        self.ref = ref
+
+
+class Select(ui.Select, ReactiveComponent):
+    def __init__(self, _callable: Callable[[discord.ui.Select], Awaitable[None]],
+                 custom_id: str = MISSING, placeholder: str | None = None, min_values: int = 1, max_values: int = 1,
+                 options: list[discord.SelectOption] = MISSING, disabled: bool = False, row: int | None = None):
         super().__init__(custom_id=custom_id, placeholder=placeholder, min_values=min_values, max_values=max_values,
                          options=options, disabled=disabled, row=row, )
+        ReactiveComponent.__init__(self)
         self.callable = _callable
 
     async def callback(self, interaction: discord.Interaction):
-        await self.callable(interaction, self)
+        self.ref.current_interaction = interaction
+        await self.callable(self)
 
 
-class Button(ui.Button):
+class Button(ui.Button, ReactiveComponent):
     def __init__(self,
-                 _callable: Callable[[discord.Interaction, discord.ui.Button], Awaitable[None]] = None,
+                 _callable: Callable[[], Awaitable[None]] = None,
                  style: discord.ButtonStyle = discord.ButtonStyle.secondary,
                  label: str | None = None,
                  disabled: bool = False,
@@ -35,15 +41,17 @@ class Button(ui.Button):
                  row: int | None = None):
         super().__init__(style=style, label=label, disabled=disabled, custom_id=custom_id, url=url, emoji=emoji,
                          row=row)
+        ReactiveComponent.__init__(self)
         self.callable = _callable
 
     async def callback(self, interaction: discord.Interaction):
+        self.ref.current_interaction = interaction
         if self.callable:
-            await self.callable(interaction, self)
+            await self.callable()
 
 
-class UserSelect(ui.UserSelect):
-    def __init__(self, _callable: Callable[[discord.Interaction, discord.ui.UserSelect], Awaitable[None]],
+class UserSelect(ui.UserSelect, ReactiveComponent):
+    def __init__(self, _callable: Callable[[discord.ui.UserSelect], Awaitable[None]],
                  custom_id: str = MISSING,
                  placeholder: str | None = None,
                  min_values: int = 1,
@@ -52,24 +60,28 @@ class UserSelect(ui.UserSelect):
                  row: int | None = None):
         super().__init__(custom_id=custom_id, placeholder=placeholder, min_values=min_values, max_values=max_values,
                          disabled=disabled, row=row)
+        ReactiveComponent.__init__(self)
         self.callable = _callable
 
     async def callback(self, interaction: discord.Interaction[ClientT]) -> Any:
-        await self.callable(interaction, self)
+        self.ref.current_interaction = interaction
+        await self.callable(self)
 
 
 class ReactiveApplicationView(ui.View):
-    last_interaction: discord.Interaction | None
+    last_interaction: discord.Interaction | None = None
+    current_interaction: discord.Interaction | None = None
     is_initial = True
-
-    async def set_state(self, interaction: discord.Interaction):
-        await self._render(interaction, to_render=self.render())
 
     def __init__(self, ephemeral: bool = True, timeout: Optional[int] = None):
         super().__init__(timeout=timeout)
         self.ephemeral = ephemeral
 
-    async def _render(self, interaction: discord.Interaction, to_render: Iterator[str | discord.Embed | ui.Item]):
+    @final
+    async def set_state(self):
+        await self._render(to_render=self.render())
+
+    async def _render(self, to_render: Iterator[str | discord.Embed | ui.Item]):
         self.clear_items()
         embed = None
         message_str = None
@@ -78,21 +90,32 @@ class ReactiveApplicationView(ui.View):
                 embed = component
             if isinstance(component, str):
                 message_str = component
-            if isinstance(component, ui.Item):
+            if isinstance(component, ui.Item) and isinstance(component, ReactiveComponent):
                 self.add_item(component)
+                component.set_ref(self)
 
-        self.last_interaction = interaction
         if self.is_initial:
-            await interaction.response.send_message(embed=embed, view=self, content=message_str,
+            await self.current_interaction.response.send_message(embed=embed, view=self, content=message_str,
                                                     ephemeral=self.ephemeral)
+            self.original_interaction = await self.current_interaction.original_response()
             self.is_initial = False
+        elif self.current_interaction:
+            await self.current_interaction.response.edit_message(embed=embed, view=self, content=message_str)
         else:
-            await interaction.response.edit_message(embed=embed, view=self, content=message_str)
+            await self.last_interaction.edit_original_response(embed=embed, view=self, content=message_str)
+        if self.current_interaction is not None:
+            self.last_interaction = self.current_interaction
+            self.current_interaction = None
 
+    @abstractmethod
     def render(self) -> Iterator[str | discord.Embed | ui.Item]:
         ...
 
 
 async def run_application(interaction: discord.Interaction, application: ReactiveApplicationView, is_initial=True):
     application.is_initial = is_initial
-    await application._render(interaction, application.render())
+    if is_initial:
+        application.current_interaction = interaction
+    else:
+        application.last_interaction = interaction
+    await application._render(application.render())
